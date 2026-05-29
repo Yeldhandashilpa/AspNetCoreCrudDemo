73fd0794e53124e10708439d30fd5424729dbb45
YeldhandaShilpa <Shilpareddy9300@gmail.com>
2026-05-29 16:55:07 +0530

    Initial ASP.NET Core CRUD Project

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using AspNetCoreCrudDemo.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AspNetCoreCrudDemo.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Student> Students { get; set; }
+    }
+}
---
+++ Models/Student.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AspNetCoreCrudDemo.Models
+{
+    public class Student
+    {
+        [Key]
+        [Display(Name="Student ID") ]
+        public int StudentId { get; set; }
+
+        [Required]
+        [Display(Name = "Student Name")]
+        public string  Name{ get; set; }
+
+        [Required]
+        public string Email  { get; set; }
+
+        [Required]
+        public string Course { get; set; }
+
+        [Display(Name = "EnrollementDate")]
+        public DateTime EnrollementDate { get; set; }
+    }
+}
